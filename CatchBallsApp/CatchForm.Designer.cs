8aaa7ec894147a28bebf5a9bef58745d360e091b
Albert <114225599+AlbertZagizulin@users.noreply.github.com>
2023-08-08 14:44:01 +0500

    Add files via upload

---
+++ CatchBallsApp/CatchForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace CatchBallsApp
+{
+    partial class CatchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.createButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // createButton
+            // 
+            this.createButton.Location = new System.Drawing.Point(19, 18);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(131, 37);
+            this.createButton.TabIndex = 0;
+            this.createButton.Text = "Create Balls";
+            this.createButton.UseVisualStyleBackColor = true;
+            this.createButton.Click += new System.EventHandler(this.createButon_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(657, 18);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(131, 37);
+            this.clearButton.TabIndex = 1;
+            this.clearButton.Text = "Clear Balls";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.scoreLabel.Location = new System.Drawing.Point(351, 27);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(59, 27);
+            this.scoreLabel.TabIndex = 2;
+            this.scoreLabel.Text = "0";
+            // 
+            // CatchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.createButton);
+            this.Name = "CatchForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.CatchForm_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.CatchForm_MouseDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button createButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label scoreLabel;
+    }
+}
+
---
+++ CatchBallsApp/CatchForm.cs
@@ -0,0 +1,74 @@
+﻿
+using BallsLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatchBallsApp
+{
+    public partial class CatchForm : Form
+    {
+        private List<RandomMoveBallBase> moveBalls;
+        private static Random random = new Random();
+        private int ballsCounter;
+
+        public CatchForm()
+        {
+            InitializeComponent();
+        }
+
+        private void createButon_Click(object sender, EventArgs e)
+        {
+            ballsCounter = 0;
+            moveBalls = new List<RandomMoveBallBase>();
+            int ballsQuantity = random.Next(5, 11);
+            for (int i = 0; i < ballsQuantity; i++)
+            {
+                var ballToList = new RandomMoveBallBase(this);
+                moveBalls.Add(ballToList);
+                moveBalls[i].Start();
+            }
+            createButton.Enabled = false;
+            clearButton.Enabled = true;
+        }
+
+        private void CatchForm_Load(object sender, EventArgs e)
+        {
+            clearButton.Enabled = false;
+        }
+
+        private void CatchForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (moveBalls != null)
+            {
+                foreach (var ball in moveBalls)
+                {
+                    if (ball.IsMoves() && ball.Contains(e.X, e.Y) && ball.IsOnForm())
+                    {
+                        ball.Stop();
+                        ballsCounter++;
+                    }
+                }
+                scoreLabel.Text = ballsCounter.ToString();
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            foreach(var ball in moveBalls)
+            {
+                ball.Clear();
+            }
+            createButton.Enabled = true;
+            clearButton.Enabled = false;
+            MessageBox.Show("Вы поймали " + ballsCounter + " из " + moveBalls.Count + " шариков");
+            scoreLabel.Text = 0.ToString();
+        }
+    }
+}
